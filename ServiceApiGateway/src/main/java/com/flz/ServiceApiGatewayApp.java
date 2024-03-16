a246ee3a14c2b5656a27468a3d809bb9c008031a
filizhelvaci <filizhelvaci82@gmail.com>
2024-03-16 11:15:33 +0300

    Api Gateway Added

---
+++ ServiceApiGateway/src/main/java/com/flz/ServiceApiGatewayApp.java
@@ -0,0 +1,12 @@
+package com.flz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApiGatewayApp {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ServiceApiGatewayApp.class, args);
+    }
+}
